0f3732d47a67b62873183c532f8627cecf207ece
prcox <prcox@calpoly.edu>
2021-01-11 17:09:35 -0800

    fourth commit

--- sample_backend.py
+++ sample_backend.py
@@ -42,11 +42,21 @@ def hello_world():
 
 @app.route('/users')
 def get_users():
-   search_username = request.args.get('name') #accessing the value of parameter 'name'
-   if search_username :
-      subdict = {'users_list' : []}
-      for user in users['users_list']:
-         if user['name'] == search_username:
-            subdict['users_list'].append(user)
-      return subdict
-   return users
+    search_username = request.args.get('name')  # accessing the value of parameter 'name'
+    if search_username:
+        subdict = {'users_list': []}
+        for user in users['users_list']:
+            if user['name'] == search_username:
+                subdict['users_list'].append(user)
+        return subdict
+    return users
+
+
+@app.route('/users/<id>')
+def get_user(id):
+    if id:
+        for user in users['users_list']:
+            if user['id'] == id:
+                return user
+        return ({})
+    return users
